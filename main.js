9d395da2e2335f6cc717849bdf565d70f28ed08e
Tim Guo <timguoqk@users.noreply.github.com>
2015-11-09 22:39:50 -0500

    finish one rotating ring

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 $(function() {
     var movie = bonsai.run(document.getElementById('main-container'), {
+        width: 500, // TODO: make it responsive
+        height: 500,
         code: originalColor
     });
 });
 
 function originalColor() {
     importScripts(self.location.origin + "/randomColor.js");
+    stage.setBackgroundColor("#8B8B8B");
     var centerX = 250,
         centerY = 250,
-        circles = 140,
+        circles = 20,
         distance = 180,
-        frames = 14,
-        radiusMin = 10,
-        radiusVar = 10;
+        fps = 30, rps = 1,
+        radius = 15;
 
     var circle, random = Math.random;
 
@@ -20,7 +22,6 @@ function originalColor() {
         var f = i / circles,
             x = centerX + distance * Math.sin(f*2*Math.PI),
             y = centerY + distance * -Math.cos(f*2*Math.PI),
-            radius = random() * radiusVar + radiusMin;
 
         circle = bonsai.Path
             .circle(x, y, radius)
@@ -30,16 +31,28 @@ function originalColor() {
         stage.addChild(circle);
     }
 
-    var c = stage.children();
-    stage.length(frames);
-    var spread = 80;
+    var children = stage.children();
+    var spread = 800;
+    var dalpha = rps * 2 * Math.PI / fps;
+    var sin_dalpha = Math.sin(dalpha), cos_dalpha = Math.cos(dalpha);
+    stage.length(fps);
     stage.on(0, function() {
-        for (var i = 0, circle; (circle = c[i++]); ) {
-
-        circle.animate(frames, {
-            x: circle.x + spread * random() - spread / 2,
-            y: circle.y + spread * random() - spread / 2
-        }, {easing: 'sineInOut'});
+        console.log('123');
+        for (var i = 0; i < children.length; i ++) {
+            var newX = centerX + (children[i].x - centerX) * cos_dalpha - (children[i].y - centerY) * sin_dalpha;
+            var newY = centerY + (children[i].x - centerX) * sin_dalpha + (children[i].y - centerY) * cos_dalpha;
+            children[i].x = newX;
+            children[i].y = newY;
+            children[i].animate(fps, {
+                x: children[i].x,
+                y: children[i].y,
+                fillColor: randomColor()
+            }, {easing: 'linear', isTimelineBound: false});
         }
     });
+
+    var center = bonsai.Path
+        .circle(centerX, centerY, 2)
+        .attr({fillColor: 'white'});
+    stage.addChild(center);
 }
